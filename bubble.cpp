82ea15c274598440906cf4e4acfe7ae9ddcb1eff
MUKUL-RAJPUT2004 <mukulrajput200705@gmail.com>
2026-03-10 16:53:41 +0530

    bubble sort

---
+++ bubble.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n){
+    // for(int i=0; i<n-1; i++){
+    //     for(int j=0; j<n-i-1;j++){
+    //         if(arr[j+1]<arr[j])    swap(arr[j+1], arr[j]);
+    //     }
+    // }
+
+    for(int i = n-1; i>=0;i--){
+        int didSwap =0;
+        for(int j=0; j<=i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                didSwap = 1;
+            }    
+        }
+        if(didSwap == 0 )    break; 
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[]={13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    bubbleSort(arr, n);
+}
